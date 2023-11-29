847dc594391694cf5b27c37864b09703e9e9cfe8
Sezer <sezer.boluk@niltekyazilim.com.tr>
2023-11-29 21:29:28 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          },
-        ],
-      },
+    images :{
+        domains: ['localhost', 'utfs.io']
+    }
 }
 
 module.exports = nextConfig
